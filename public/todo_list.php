5094933a7ac78816e243a5311b5a9bc66f6356be
James Garcia <james.m.garcia7@gmail.com>
2014-10-15 09:32:20 -0500

    todo list

---
+++ public/todo_list.php
@@ -0,0 +1,34 @@
+<?php
+
+if ($_GET) {
+	var_dump($_GET);
+}
+
+if ($_POST) {
+	var_dump($_POST);
+}
+
+
+	?>
+
+
+<html>
+	<title>TODO List</title>
+		<body>
+			<h1>TODO List</h1>
+				<ul>
+					<li>do this</li>
+					<li>clean kitchen</li>
+					<li>sleep</li>
+				</ul>
+				<hr>
+				<h2>Add items TODO list</h2>
+				<form method="POST" action="/todo_list.html">
+					
+					<label for="todo_items">New Item</label>
+					<input type="text" id="todo_items" name="todo_items" placeholder="Add item to todo list">
+					<input type="submit" value="Add!">
+				
+				</form>
+		</body>		
+</html>
