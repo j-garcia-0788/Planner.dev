9734dff695165971128b1ef092dee8ff1b4204b0
James Garcia <james.m.garcia7@gmail.com>
2014-11-18 11:43:41 -0600

    throw/catches

--- public/address_book.php
+++ public/address_book.php
@@ -1,57 +1,52 @@
-
 <?php
 
-require_once '../inc/address_data_store.php'; 
+require_once '../inc/address_data_store.php';
+require_once '../inc/filestore.php';
 
-	$addbook = new AddressDataStore();
-	$addbook->filename = 'address2.csv';
-	$addresses = $addbook->readCSV();
+	$addbook = new AddressDataStore('address2.csv');
+
+	$addresses = $addbook->read();
 	$addbook->array=$addresses;
 
-	
-	
+
 	if (isset($_GET['id'])) {
 		$id = $_GET['id'];
 		unset($addresses[$id]);
-		$addbook->writeCSV($addresses);
+		$addbook->write($addresses);
 	}
 
 	if (!empty($_POST)) {
+		
+		try {
+			
+			$addbook->validate($_POST);
+			if (empty($_POST['name']) || empty($_POST['address']) || empty($_POST['city']) || empty($_POST['state'])
+					 || empty($_POST['zip']) || empty($_POST['phone'])) {
+				
 
+			} else {
+				
+					$newEntry['name'] = $_POST['name'];
+		
+					$newEntry['address'] = $_POST['address'];		
+				
+					$newEntry['city'] = $_POST['city'];
+				
+					$newEntry['state'] = $_POST['state'];
+				
+					$newEntry['zip'] = $_POST['zip'];
+				
+					$newEntry['phone'] = $_POST['phone'];
+				
+				
+				$addresses[]=$newEntry;
+				$addbook->write($addresses);
+			}
+			} catch (Exception $e) {
+	    			$error = $e->getMessage();
+    		}
+		}
 	
-	if (empty($_POST['name']) || empty($_POST['address']) || empty($_POST['city']) || empty($_POST['state'])
-			 || empty($_POST['zip']) || empty($_POST['phone'])) {
-		$error = "Please input all fields.";
-	} else {
-		
-		// Only the Name from the form is getting set to the new entry variable
-		//- which is causing part of your problem.
-		if (isset($_POST['name'])) {
-			$newEntry['name'] = $_POST['name'];
-		}
-		if (isset($_POST['address'])) {
-			$newEntry['address'] = $_POST['address'];
-		}
-		if (isset($_POST['city'])) {
-			$newEntry['city'] = $_POST['city'];
-		}
-		if (isset($_POST['state'])) {
-			$newEntry['state'] = $_POST['state'];
-		}
-		if (isset($_POST['zip'])) {
-			$newEntry['zip'] = $_POST['zip'];
-		}
-		if (isset($_POST['phone'])) {
-			$newEntry['phone'] = $_POST['phone'];
-		}
-
-		$addresses[]=$newEntry;
-		//$Address_book_obj->write_to_csv ($addressBook);
-		
-		$addbook->writeCSV($addresses);
-	}
-}	
-
 ?>
 
 <html>
@@ -68,16 +63,36 @@ require_once '../inc/address_data_store.php';
 
 </head>
 <body>
-	<h1>Contacts</h1>
-	<p>============================</p>
-	
+<div class="container">
+<h1>Address Book</h1>
+<p>==========================================</p>
 	<? if(isset($error)): ?>
-	<div class="alert alert-warning alert-dismissible" role="alert">
-  		<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span>
-  		<span class="sr-only">Close</span></button>
+	<div class="alert alert-warning" role="alert">
   		<p> <?= $error ?> </p>
 	</div>
 	<? endif; ?>
+	<h3>Enter Contact</h3>
+	<p>==========================================================================================================================================================</p>
+
+	<form role = "form" class="form-horizontal" method="POST" action="address_book.php">
+
+		<input type="text" id="name" name="name" placeholder="Enter Name">
+			
+		<input id="address" name="address" placeholder="Enter Address Here">
+
+		<input id="city" name="city" placeholder="Enter City Here">
+	
+		<input id="state" name="state" placeholder="Enter State Here">
+
+		<input id="zip" name="zip" placeholder="Enter Zip Here">
+
+		<input type="text" id="phone" name="phone" placeholder="(555) 555 - 5555">
+		<br><br>
+		<button type="submit" class="btn btn-info">Add</button>
+	</form>
+	<h3>Contact Info</h3>
+	<p>==========================================================================================================================================================</p>
+	
 	<div class="table">
 	<table class="table table-hover">
 		<tr> 
@@ -90,14 +105,11 @@ require_once '../inc/address_data_store.php';
 			<th>Phone:</th>
 			<th> </th>
 		</tr>
-	</div>
-	<div class="table">	
 		<?  foreach ($addresses as $key => $address): ?>
 			<tr>
 			
 			<?foreach ($address as $value): ?>
-				<!--var_dump($value);-->
-				<!-- insert each in table row -->
+				
 				<td><?= htmlspecialchars(strip_tags($value)); ?></td>
 
 			<? endforeach; ?>
@@ -106,30 +118,8 @@ require_once '../inc/address_data_store.php';
 			</tr>	
 			
 		<? endforeach; ?>
-
 	</table>
+	</div>
 </div>
-	<h2>Enter Contact</h2>
-	<p>============================</p>
-
-	<form id = "form" role = "form" class="form-inline" method="POST" action="address_book.php">
-
-			<input type="text" id="name" name="name" placeholder="Enter Name">
-			
-			<input id="address" name="address" placeholder="Enter Address Here">
-
-			<input id="city" name="city" placeholder="Enter City Here">
-		
-			<input id="state" name="state" placeholder="Enter State Here">
-	
-			<input id="zip" name="zip" placeholder="Enter Zip Here">
-
-			<input type="text" id="phone" name="phone" placeholder="Enter Phone Here">
-			<br><br>
-			<button type="submit" class="btn">Add</button>
-
-	</form>
-
-
 </body>
 </html>
--- public/index.php
+++ public/index.php
@@ -3,97 +3,103 @@
 
 require_once '../inc/filestore.php';
 
-			$list = new Filestore();
-			$list->filename = 'data/list.txt';
-			$all_items = $list->readLines();
-			//$list->items = $list->openfile();
-			// $list->filename = 'data/list.txt';
-			// $list->items = $all_items;
-			// $list->savefile();
-		 
-		// Check for GET Requests
-		    // If there is a get request; remove the appropriate item.
+	$list = new Filestore();
+	$list->filename = 'data/list.txt';
+	$all_items = $list->read();
 		
-			if (isset($_GET['id'])) {
-				$id = $_GET['id'];
-			 	unset($all_items[$id]);
-			 	$list->todos = $all_items;
-			 	$list->writeLines($all_items);
-			 	}	
-		// Check for POST Requests
-	   	// If there is a post request; add the items.
+	if (isset($_GET['id'])) 
+	{
+		$id = $_GET['id'];
+	 	unset($all_items[$id]);
+	 	$list->todos = $all_items;
+	 	$list->write($all_items);
+	 }	
+	// Check for POST Requests and post new items
 
-			if (isset($_POST['newitem'])) {
-				//var_dump($_POST);
-				$all_items[] = $_POST['newitem'];
-				$list->todos = $all_items;
-				//$items[] = $itemToAdd;
-				//echo "<p>item to add: " . $_POST ['newitem'] . "</p>"
-				$list->writeLines($all_items);
-			}
+	if (isset($_POST['newitem'])) 
+	{
+		try {
 			
-		// Verify there were uploaded files and no errors
+			if (strlen($_POST['newitem']) == 0) {
+	    		throw new Exception('This can not empty');
+	    	}
+	    	if (strlen($_POST['newitem']) > 240) {
+	    		throw new Exception('This can not be over 240 characters');
+			}
+			$all_items[] = $_POST['newitem'];
+			$list->todos = $all_items;
+			$list->write($all_items);
+    	
+    	} catch (Exception $e) {
+    		echo $e->getMessage();
+    	}
+
+	}
+			
+	// Verify there were uploaded files and no errors
 	
-			if (count($_FILES) > 0 && $_FILES['file1']['error'] == UPLOAD_ERR_OK) {
-					 // Set the destination directory for uploads
-					 $uploadDir = '/vagrant/sites/planner.dev/public/uploads/';
+	if (count($_FILES) > 0 && $_FILES['file1']['error'] == UPLOAD_ERR_OK) 
+	{
+		// Set the destination directory for uploads
+		$uploadDir = '/vagrant/sites/planner.dev/public/uploads/';
 
-					 // Grab the filename from the uploaded file by using basename
-					 $filename = basename($_FILES['file1']['name']);
+		// Grab the filename from the uploaded file by using basename
+		$filename = basename($_FILES['file1']['name']);
 
-					 // Create the saved filename using the file's original name and our upload directory
-					 $savedFilename = $uploadDir . $filename;
+		// Create the saved filename using the file's original name and our upload directory
+		$savedFilename = $uploadDir . $filename;
 
-					  // Move the file from the temp location to our uploads directory
-					  move_uploaded_file($_FILES['file1']['tmp_name'], $savedFilename);
+		// Move the file from the temp location to our uploads directory
+		move_uploaded_file($_FILES['file1']['tmp_name'], $savedFilename);
 
-					  //check for text file
-					  if ($_FILES['file1']['type'] == 'text/plain') {
-					  	$newitems = $list->readLines($savedFilename);
-					  	array_merge($this->todos, $newitems);
-					  	$list->writeLines($newitems);
+		//check for text file
+		if ($_FILES['file1']['type'] == 'text/plain') 
+		{
+			$newitems = $list->read($savedFilename);
+		  	array_merge($this->todos, $newitems);
+		  	$list->write($newitems);
 
-					  }
-				}	
-		?>
+		}
+	}	
+?>
  
 <html>
 <head>
     <title>TODO App</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/bootstrap/3.3.0/css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/todo.css">
-
 	<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://cdn.jsdelivr.net/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 </head>
 <body class="body">
 
-<h1>ToDo List.</h1> 
+<h1>ToDo List.</h1><p>====================</p><br>
 
 <div class="stuff">
 	<ol>
 		<? foreach ($all_items as $key => $entry): ?>
 			<li>				
-				<?= $entry ?><a href="?id=<?=$key; ?>"> Done</a>
+				<?= $entry ?><a href="?id=<?=$key; ?>">  <button type="button" class="btn btn-danger btn-xs">x</button></a><br>
 			</li>
 		<? endforeach ?>	
 	</ol>
-<!-- Create a Form to Accept New Items -->
+
+	<br>
 	<form method="POST" name="add-form" action="index.php">
 		
-		<label for="newitem">Add an item lil homie:</label>
+		<label for="newitem">Add an item to do:</label>
 		
 		<input id="newitem" name="newitem" type="text">
 
- 		<button type="submit">Add</button>
+ 		<button type="submit" class="btn btn-primary btn-s">Add</button>
 
  		</form>
  		<h3>===========================================</h3>
  		<h1>Upload file</h1>
 	<?php
 	    // Check if we saved a file
-	    if (isset($savedFilename)) {
+	    if (isset($savedFilename)) 
+	    {
 	        echo "<p>You can download your file <a href='/uploads/{$filename}'>here</a>.</p>";
 	    }
 	    ?>
